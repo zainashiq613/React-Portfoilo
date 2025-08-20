a4a4a2ab2f5e5b143e290bd10969a418e0d8190d
Muhammad Zain <zain.ashiq613@gmail.com>
2025-08-20 10:13:39 +0500

    completed

--- src/pages/layout/Footer.jsx
+++ src/pages/layout/Footer.jsx
@@ -2,9 +2,9 @@ function Footer() {
   return (
     <section className="relative py-5">
       <section className="mt-3 rounded-lg p-4 flex items-center border border-[#EEEEEE] bg-white justify-between max-w-[1440px] mx-auto">
-        <div className="bg-primary rounded-lg w-full flex items-center justify-between px-5 py-2">
-          <div className="text-white">h.mhammas@gmail.com</div>
-          <div className="flex gap-2.5">
+        <div className="bg-primary rounded-lg w-full flex flex-col sm:flex-row gap-5 sm:items-center justify-between px-5 py-2">
+          <div className="text-white text-sm">h.mhammas@gmail.com</div>
+          <div className="flex justify-end gap-2.5">
             <div>
               <img src="/facebook 1.png" alt="" />
             </div>
